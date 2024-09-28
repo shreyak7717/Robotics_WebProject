282974a982b973f1a94c1d143a2b4603831a3bf9
PratyakshSaluja <pratyakshsirsa@gmail.com>
2024-09-28 12:34:26 +0530

    routes folder

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express()
 app.set("view engine", "ejs");
+const routes = require('./routes/routes'); // Import routes from routes.js
 app.set("views", __dirname + '/views');
 app.use(express.static('./assets'));
 app.use(express.static('./scripts'));
 app.use(express.static('./apps'));
 
-app.get('/',(req,res)=>{
-    res.render('resources')
-})
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+
+app.use('/',routes);
+
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log("Error connecting with server", error);
+  } else {
+    console.log(`Server is listening on port -> ${PORT}`);
+    console.log(`\n\nhttp://localhost:${PORT}\n\n`);
+  }
 });
